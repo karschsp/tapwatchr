274568e236b93833d9939bd69520baf445a5eebb
Steve Karsch <steve@stevekarsch.com>
2012-08-28 15:58:40 -0400

    Add beer_save() to write out to .json files.  Modify index.php to actually display taps/beers.

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,11 @@
+<?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 'On');
+
+?>
 <html>
   <head>
-    <title>TapWatch</title>
+    <title>Tapwatchr</title>
     <style type="text/css">
 
     </style>
@@ -10,10 +15,24 @@
   
   </head>
   <body>
+    <h1>Tapwatchr</h1>
     <form name="signup" id="signup" method="POST" action="signup.php">
       <input type="email" id="email" name="email" placeholder="you@example.com" />
       <input type="submit" value="Signup" />
     </form>
-    <p>beer...the cause of, and solution to, all of life's problems.</p>
+    <h2>Taps we're watching</h2>
+    <?php
+    $taps_json = file_get_contents('../json/taps.json');
+    $taps = json_decode($taps_json);
+    foreach ($taps->Taps as $tap) {
+      print '<h3>' . $tap->Name . '</h3>';
+      $tap_json = file_get_contents('../json/' . $tap->ShortName . '.json');
+      $beers = json_decode($tap_json);
+      foreach ($beers as $beer) {
+        print '<li>' . $beer . '</li>';
+      }
+    }
+    ?>
+    <blockquote>beer...the cause of, and solution to, all of life's problems.</blockquote>
   </body>
 </html>
