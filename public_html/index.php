030f03dccff7e20dcfa039904bb546d5dca92093
Steve Karsch <steve@stevekarsch.com>
2012-08-29 10:42:23 -0400

    Add the twitter follow button to the homepage.

--- public_html/index.php
+++ public_html/index.php
@@ -34,5 +34,7 @@ ini_set('display_errors', 'On');
     }
     ?>
     <blockquote>beer...the cause of, and solution to, all of life's problems.</blockquote>
+<a href="https://twitter.com/tapwatchr" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @tapwatchr</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
   </body>
 </html>
