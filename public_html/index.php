ec4097e941899a834ba64d66ce183a17d616be93
Steve Karsch <steve@stevekarsch.com>
2012-08-30 08:33:28 -0400

    Adding google analytics to index.php

--- public_html/index.php
+++ public_html/index.php
@@ -42,6 +42,20 @@ ini_set('display_errors', 'On');
         }
         ?>
       </div>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-34427877-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
     <footer>
       <blockquote>beer...the cause of, and solution to, all of life's problems.</blockquote>
 <a href="https://twitter.com/tapwatchr" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @tapwatchr</a>
